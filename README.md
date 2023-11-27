3e8279f56c51a6dbb1ac84cf1b2fae2914700df1
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2023-11-27 07:33:20 +0700

    Create initial files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+A simple drawing app, made for The Odin Project assignment.
---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+
+/*
+for(let i=0; i<16; i++) {
+    div = document.createElement('div');
+    
+    container.appendChild();
+}
+*/
